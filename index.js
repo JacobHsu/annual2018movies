1ef408010527c21870e8c898699a2e341d65e515
jacobhsu <jacob.hsu.tw@gmail.com>
2018-09-17 21:12:57 +0800

    https://github.com/axios/axios

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    // sections: [
+    //     {
+    //     bgColor: '#ec407a',
+    //     content: 'I am section 1',
+    //     img_url: './imgs/bg1.jpg'
+    //   },
+    //   {
+    //     bgColor: '#42a5f5',
+    //     content: 'I am section 2',
+    //     img_url: './imgs/bg2.jpg'
+    //   },
+    //   {
+    //     bgColor: '#66bb6a',
+    //     content: 'I am section 3',
+    //     img_url: './imgs/bg3.jpg'
+    //   }
+    // ],
+    sections: [],
+    options: {
+      // Your custom options here
+      duration: 800,
+      overlay: false
+    }
+  },
+  mounted() {
+    axios.get("/movies.json")
+    .then(response => {this.sections = response.data;})
+  }
+});
